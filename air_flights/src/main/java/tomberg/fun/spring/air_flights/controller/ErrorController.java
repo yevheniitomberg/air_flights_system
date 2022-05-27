934611db42dcee5ec2ef85324d46e508883e48c4
Yevhenii Tomberg <yevhenii@tomberg.com>
2022-05-27 22:00:16 +0300

    404 error view was added

--- air_flights/src/main/java/tomberg/fun/spring/air_flights/controller/ErrorController.java
+++
@@ -1,8 +0,0 @@
-package tomberg.fun.spring.air_flights.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class ErrorController {
-    // This class will use for handling error all over the project
-}
